ebd93bd09335593b2a33a91e20570f12c8a17f00
Jonathan Fielding <j.fielding@me.com>
2015-12-10 21:52:45 +0000

    Provide useful suggestions for similar apis

--- app/controllers/v1/feature/feature-controller.js
+++ app/controllers/v1/feature/feature-controller.js
@@ -21,8 +21,12 @@ FeatureController.prototype = {
 			res.status(200).json(response);
 		}
 		catch (err) {
+
+			var closest = caniuse.find(feature);
+
 			res.status(404).json({
-				error: 'api not found'
+				error: 'api not found',
+				suggestions: closest
 			});
 		}
 	}
