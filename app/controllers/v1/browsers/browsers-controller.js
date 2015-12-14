7af66b3462b7d1c89ea7b63255029f9007066de8
Jonathan Fielding <j.fielding@me.com>
2015-12-14 08:06:06 +0000

    update how the data is parsed for latest browser versions, a nice object instead of horrible array that the user would have to parse

--- app/controllers/v1/browsers/browsers-controller.js
+++ app/controllers/v1/browsers/browsers-controller.js
@@ -5,10 +5,20 @@ function BrowsersController() {
 
 BrowsersController.prototype = {
 	get: function(req, res, next) {
-		var browsers = caniuse.getLatestStableBrowsers();
+		var browserData = caniuse.getLatestStableBrowsers();
+		var joinedBrowsers = browserData.join(',');
 
 		var response = {
-			browsers: browsers,
+			browsers: {
+				chrome: joinedBrowsers.match('chrome ([0-9\.]{1,3})') && joinedBrowsers.match('chrome ([0-9\.]{1,3})')[1],
+				edge: joinedBrowsers.match('edge ([0-9\.]{1,3})') && joinedBrowsers.match('edge ([0-9\.]{1,3})')[1],
+				ie: joinedBrowsers.match('ie ([0-9\.]{1,3})') && joinedBrowsers.match('ie ([0-9\.]{1,3})')[1],
+				ie_mob: joinedBrowsers.match('ie_mob ([0-9\.]{1,3})') && joinedBrowsers.match('ie_mob ([0-9\.]{1,3})')[1],
+				ios_saf: joinedBrowsers.match('ios_saf ([0-9\.]{1,3})') && joinedBrowsers.match('ios_saf ([0-9\.]{1,3})')[1],
+				firefox: joinedBrowsers.match('firefox ([0-9\.]{1,3})') && joinedBrowsers.match('firefox ([0-9\.]{1,3})')[1],
+				opera: joinedBrowsers.match('opera ([0-9\.]{1,3})') && joinedBrowsers.match('opera ([0-9\.]{1,3})')[1],
+				safari: joinedBrowsers.match('safari ([0-9\.]{1,3})') && joinedBrowsers.match('safari ([0-9\.]{1,3})')[1]
+			},
 			attribution: 'Licensed under CC BY 4.0 license from caniuse.com'
 		}
 
